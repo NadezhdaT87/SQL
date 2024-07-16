b69d2424197c74099103b31ef9b09d37f9ed59e6
NadezhdaT <kybalibri@mail.ru>
2024-07-16 17:35:36 +0700

    initial commit

---
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -0,0 +1,49 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+    LoginPage loginPage;
+    @AfterEach
+    void tearDown(){
+        cleanAuthCodes();
+    }
+    @AfterAll
+    static void tearDownAll(){
+        cleanDatabase();
+    }
+    @BeforeEach
+    void setUp(){
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+    @Test
+    void shouldSuccessfulLogin(){
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+    @Test
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase(){
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+    @Test
+    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode(){
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! \nНеверно указан код! Попробуйте ещё раз.");
+    }
+}
